107ed67604ad37accd32e88973c6f583467c776d
Christopher Jacob Wood <jacob@Christophers-MacBook-Pro.local>
2017-06-11 12:05:54 -0600

    Constructors, getters and setters for Jet Class

--- src/jetapp/Jet.java
+++ src/jetapp/Jet.java
@@ -6,4 +6,62 @@ public class Jet {
 	private int range;
 	private double price;
 
+	public Jet() {
+		super();
+	}
+
+	public Jet(String aircraftModel, float speed, int range, double price) {
+		super();
+		this.aircraftModel = aircraftModel;
+		this.speed = speed;
+		this.range = range;
+		this.price = price;
+	}
+
+	public String getAircraftModel() {
+		return aircraftModel;
+	}
+
+	public void setAircraftModel(String aircraftModel) {
+		this.aircraftModel = aircraftModel;
+	}
+
+	public float getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(float speed) {
+		this.speed = speed;
+	}
+
+	public int getRange() {
+		return range;
+	}
+
+	public void setRange(int range) {
+		this.range = range;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Jet [aircraftModel=");
+		builder.append(aircraftModel);
+		builder.append(", speed=");
+		builder.append(speed);
+		builder.append(", range=");
+		builder.append(range);
+		builder.append(", price=");
+		builder.append(price);
+		builder.append("]");
+		return builder.toString();
+	}
 }
