e280f815633fc77f6b8309492b1a10a214cb3982
Christopher Jacob Wood <jacob@Christophers-MacBook-Pro.local>
2017-06-11 16:13:33 -0600

    Created JetAppMenu with methods for user choice

--- src/jetapp/JetAppDriver.java
+++ src/jetapp/JetAppDriver.java
@@ -4,5 +4,60 @@ import java.util.Scanner;
 
 public class JetAppDriver {
 	Scanner kb = new Scanner(System.in);
-	
+	AircraftCarrier carrier = new AircraftCarrier();
+
+	public void startJetApp() {
+		jetAppMenuInput();
+	}
+
+	private void jetAppMenuInput() {
+		boolean quitApp = false;
+
+		while (!quitApp) {
+			System.out.println("Enter your choice: ");
+			int userInput = kb.nextInt();
+			quitApp = userChoice(userInput);
+		}
+
+	}
+
+	private boolean userChoice(int choice) {
+
+		switch (choice) {
+
+		case 1:
+			listFleet();
+			break;
+
+		case 2:
+			viewFastestJet();
+			break;
+
+		case 3:
+			viewLongestRange();
+			break;
+
+		case 4:
+			carrier.addJetToFleet();
+			break;
+			
+		case 5:
+			System.out.println("Thank you for using JetApp 1.0/n Quitting!");
+			return true;
+
+		}
+		return false;
+	}
+
+	public void listFleet() {
+
+	}
+
+	public void viewFastestJet() {
+
+	}
+
+	public void viewLongestRange() {
+
+	}
 }
