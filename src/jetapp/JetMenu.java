2d37ff9ea55f77233c3137b73415519b7e01c8b5
Christopher Jacob Wood <jacob@Christophers-MacBook-Pro.local>
2017-06-11 12:50:39 -0600

    Jet Menu completed

--- src/jetapp/JetMenu.java
+++ src/jetapp/JetMenu.java
@@ -4,5 +4,24 @@ public class JetMenu {
 
 	public void menuOptions(){
 		
+		System.out.println("*************************************************");
+		System.out.println("*************************************************");
+		System.out.println("*******            Fleet Menu             *******");
+		System.out.println("*************************************************");
+		System.out.println("***                                           ***");
+		System.out.println("***       1. List Jets Fleet                  ***");
+		System.out.println("***                                           ***");
+		System.out.println("***       2. View fastest jet                 ***");
+		System.out.println("***                                           ***");
+		System.out.println("***       3. View jet with longest range      ***");
+		System.out.println("***                                           ***");
+		System.out.println("***       4. Add a jet to fleet               ***");
+		System.out.println("***                                           ***");
+		System.out.println("***       5. Quit                             ***");
+		System.out.println("***                                           ***");
+		System.out.println("***                                           ***");
+		System.out.println("*************************************************");
+		System.out.println("*************************************************");
+	
 	}
 }
